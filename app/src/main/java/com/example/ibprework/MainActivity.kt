04969c44895f1b9a9ae7402610e5d52ac5652610
ilabet <ilabet36@gmail.com>
2025-10-23 01:01:00 -0400

    Initial commit

---
+++ app/src/main/java/com/example/ibprework/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.ibprework
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            Log.v(TAG, "Button Cleared!")
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
+//NOTE TO TEACHER: I didn't know that we were supposed to connect Android Studio to Github!
